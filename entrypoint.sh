8f2f4b21960137ad20252ddb775faaa5f2494573
ganga.tamang <ganga.bomzan@gmail.com>
2022-05-30 11:07:49 -0400

    main changed

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -l
-
-echo "Hello $1"
-echo "Hello another $2"
+echo "The first first argument is 0 : $0" 
+echo "The second argument is 1  :  $1"
+echo "Hello another  argument is 2 : $2"
 time=$(date)
 echo "::set-output name=time::$time"
 echo "::set-output name=another::$2"
